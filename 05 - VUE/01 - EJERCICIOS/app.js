44d8b25d491effdf90170d986b5448d6ecdf008f
jaimechacon11 <chacon.jaime99g@gmail.com>
2022-03-17 15:14:33 -0600

    Intro VUE

---
+++ 05 - VUE/01 - EJERCICIOS/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            nombre: 'Rodrigo',
+            apellido: "Martinez",
+            email: "rmartinez@innovaccion.com",
+            genero: "male",
+            foto: 'https://randomuser.me/api/portraits/men/75.jpg'
+        }
+    },
+    methods:{
+        async cambiar_usuario(){
+            const res = await fetch('https://randomuser.me/api/');
+            const { results } = await res.json();
+            console.log(results);
+
+
+            this.nombre = results[0].name.first,
+            this.apellido = results[0].name.last,
+            this.email = results[0].email,
+            this.genero = results[0].gender,
+            this.foto = results[0].picture.large
+        }
+    }
+})
+
+app.mount('#app')
